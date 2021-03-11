192ec40a3ae2ba85eadf0b62f97972e8b182706a
IvanSR <navi.sr.2018@gmail.com>
2021-03-11 17:40:14 -0600

    build

---
+++ precache-manifest.f014e025a5818788d9209ce401deb287.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb7b82b79988718d5d3cde9c4e0f7d76",
+    "url": "/19feb2/index.html"
+  },
+  {
+    "revision": "6878fa3666cb751497af",
+    "url": "/19feb2/static/css/main.d03163bd.chunk.css"
+  },
+  {
+    "revision": "20a434f3cd89dedc8886",
+    "url": "/19feb2/static/js/2.68d01a0a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/19feb2/static/js/2.68d01a0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6878fa3666cb751497af",
+    "url": "/19feb2/static/js/main.8e553fca.chunk.js"
+  },
+  {
+    "revision": "be906c794344fa565297",
+    "url": "/19feb2/static/js/runtime-main.136e620d.js"
+  }
+]);
